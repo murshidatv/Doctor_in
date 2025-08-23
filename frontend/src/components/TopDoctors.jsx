33f8f3a0e506fcd2eb278c8952c65554f8696b1f
Murshida <murshidatv@gmail.com>
2025-08-23 16:39:26 +0530

    second commit

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import { doctors } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+
 const TopDoctors = () => {
 
 const navigate = useNavigate()
@@ -17,7 +18,7 @@ const navigate = useNavigate()
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 "
             key={index}
           >
-            <img className="bg-blue-50" src={item.image} alt="" />
+            <img className="bg-blue-50" src={item.image} alt={item.name} />
             <div className="p-4">
               <div className="flex items-center gap-2 text-sm text-center text-green-500">
                 <p className="w-2 h-2 bg-green-500 rounded-full"></p>
@@ -30,7 +31,7 @@ const navigate = useNavigate()
           </div>
         ))}
       </div>
-      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">
+      <button onClick={()=>{navigate('/doctors');scrollTo(0,0)}} className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">
         more
       </button>
     </div>
