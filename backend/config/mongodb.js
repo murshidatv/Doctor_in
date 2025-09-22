06c185baff6cea7e9a8554237857ed07947f1d48
Murshida <murshidatv@gmail.com>
2025-09-22 23:37:19 +0530

    backend started

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,19 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    mongoose.connection.on('conected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/doctorin`)
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/doctorin`);
+console.log("Database Connected")
+    }
+      
+catch (err) {
+    console.error(' Could not connect to MongoDB:', err.message);
+    process.exit(1); // exit if connection fails
+  }
+
+
+
 }
+
 
 export default connectDB
